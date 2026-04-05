f6e2da8ede5b1dec9667aec5dacce449f8cd7145
JunichiSugiura <jun.sugiura.jp@gmail.com>
2026-04-05 20:54:46 +0200

    Improved os menu macro

--- crates/vmux_macro/src/lib.rs
+++ crates/vmux_macro/src/lib.rs
@@ -1,57 +1,64 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{Attribute, Data, DeriveInput, Fields, parse_macro_input};
+use syn::{Attribute, Data, DeriveInput, Fields, LitStr, parse_macro_input};
 
-#[proc_macro_derive(NativeMenuLeaf)]
-pub fn derive_native_menu_leaf(input: TokenStream) -> TokenStream {
+#[proc_macro_derive(OsSubMenu, attributes(menu))]
+pub fn derive_os_sub_menu(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    match impl_native_menu_leaf(input) {
+    match impl_os_sub_menu(input) {
         Ok(tokens) => tokens.into(),
         Err(e) => e.to_compile_error().into(),
     }
 }
 
-#[proc_macro_derive(NativeMenu)]
-pub fn derive_native_menu(input: TokenStream) -> TokenStream {
+#[proc_macro_derive(OsMenu, attributes(menu))]
+pub fn derive_os_menu(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    match impl_native_menu(input) {
+    match impl_os_menu(input) {
         Ok(tokens) => tokens.into(),
         Err(e) => e.to_compile_error().into(),
     }
 }
 
-fn impl_native_menu_leaf(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+fn impl_os_sub_menu(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let ident = &input.ident;
     let Data::Enum(data) = &input.data else {
         return Err(syn::Error::new_spanned(
             ident,
-            "NativeMenuLeaf only supports enums",
+            "OsSubMenu only supports enums",
         ));
     };
 
     let mut items = Vec::new();
     let mut item_refs = Vec::new();
+    let mut from_menu_arms = Vec::new();
+
     for (idx, variant) in data.variants.iter().enumerate() {
         let Fields::Unit = &variant.fields else {
             return Err(syn::Error::new_spanned(
                 &variant.fields,
-                "NativeMenuLeaf only supports unit enum variants",
+                "OsSubMenu only supports unit enum variants",
             ));
         };
-        let props = StrumProps::from_attrs(&variant.attrs)?;
+        let props = MenuProps::from_attrs(&variant.attrs)?;
         let (Some(id), Some(label)) = (&props.id, &props.label) else {
             return Err(syn::Error::new_spanned(
                 variant,
-                "each variant needs #[strum(props(Id = \"...\", Label = \"...\"))]",
+                "each variant needs #[menu(id = \"...\", label = \"...\")]",
             ));
         };
-        let id = id.as_str();
+        let id_lit = id.as_str();
         let label = label.as_str();
-        let item_ident = format_ident!("native_menu_item_{}", idx);
+        let item_ident = format_ident!("os_menu_item_{}", idx);
         items.push(quote! {
-            let #item_ident = ::muda::MenuItem::with_id(#id, #label, true, None);
+            let #item_ident = ::muda::MenuItem::with_id(#id_lit, #label, true, None);
         });
         item_refs.push(quote! { &#item_ident });
+
+        let variant_ident = &variant.ident;
+        from_menu_arms.push(quote! {
+            #id_lit => ::core::option::Option::Some(#ident::#variant_ident),
+        });
     }
 
     Ok(quote! {
@@ -63,46 +70,52 @@ fn impl_native_menu_leaf(input: DeriveInput) -> syn::Result<proc_macro2::TokenSt
                 submenu.append_items(&[#(#item_refs),*])?;
                 Ok(())
             }
+
+            pub fn from_menu_id(id: &str) -> ::core::option::Option<Self> {
+                match id {
+                    #(#from_menu_arms)*
+                    _ => ::core::option::Option::None,
+                }
+            }
         }
     })
 }
 
-fn impl_native_menu(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+fn impl_os_menu(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let ident = &input.ident;
     let Data::Enum(data) = &input.data else {
-        return Err(syn::Error::new_spanned(
-            ident,
-            "NativeMenu only supports enums",
-        ));
+        return Err(syn::Error::new_spanned(ident, "OsMenu only supports enums"));
     };
 
     let mut submenu_blocks = Vec::new();
     let mut submenu_idents = Vec::new();
+    let mut from_menu_clauses = Vec::new();
 
     for variant in &data.variants {
         let Fields::Unnamed(fields) = &variant.fields else {
             return Err(syn::Error::new_spanned(
                 &variant.fields,
-                "NativeMenu expects tuple variants like Space(SpaceCommand)",
+                "OsMenu expects tuple variants like Space(SpaceCommand)",
             ));
         };
         let Some(field) = fields.unnamed.first() else {
             return Err(syn::Error::new_spanned(
                 variant,
-                "NativeMenu tuple variant needs one field",
+                "OsMenu tuple variant needs one field",
             ));
         };
         let inner_ty = &field.ty;
-        let props = StrumProps::from_attrs(&variant.attrs)?;
+        let props = MenuProps::from_attrs(&variant.attrs)?;
         let Some(title) = props.label.clone() else {
             return Err(syn::Error::new_spanned(
                 variant,
-                "each variant needs #[strum(props(Label = \"...\"))] for the submenu title",
+                "each variant needs #[menu(label = \"...\")] for the submenu title",
             ));
         };
+        let variant_ident = &variant.ident;
         let submenu_ident = syn::Ident::new(
             &format!(
-                "{}_native_submenu",
+                "{}_os_submenu",
                 heck_variant_snake_case(&variant.ident.to_string())
             ),
             variant.ident.span(),
@@ -112,9 +125,24 @@ fn impl_native_menu(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream>
             let mut #submenu_ident = ::muda::Submenu::new(#title, true);
             <#inner_ty>::append_native_menu_leaf(&mut #submenu_ident)?;
         });
+
+        from_menu_clauses.push(quote! {
+            <#inner_ty>::from_menu_id(id).map(#ident::#variant_ident)
+        });
     }
 
     let submenu_refs: Vec<_> = submenu_idents.iter().map(|i| quote! { &#i }).collect();
+
+    let from_menu_body = if from_menu_clauses.is_empty() {
+        quote! { ::core::option::Option::None }
+    } else {
+        let first = &from_menu_clauses[0];
+        let chained = from_menu_clauses[1..].iter().fold(
+            quote! { #first },
+            |acc, c| quote! { #acc.or_else(|| #c) },
+        );
+        quote! { #chained }
+    };
 
     Ok(quote! {
         impl #ident {
@@ -132,40 +160,39 @@ fn impl_native_menu(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream>
                 ])?;
                 Ok(())
             }
+
+            pub fn from_menu_id(id: &str) -> ::core::option::Option<Self> {
+                #from_menu_body
+            }
         }
     })
 }
 
-struct StrumProps {
+struct MenuProps {
     id: Option<String>,
     label: Option<String>,
 }
 
-impl StrumProps {
+impl MenuProps {
     fn from_attrs(attrs: &[Attribute]) -> syn::Result<Self> {
         let mut id = None;
         let mut label = None;
         for attr in attrs {
-            if !attr.path().is_ident("strum") {
+            if !attr.path().is_ident("menu") {
                 continue;
             }
             attr.parse_nested_meta(|meta| {
-                if meta.path.is_ident("props") {
-                    meta.parse_nested_meta(|inner| {
-                        if inner.path.is_ident("Id") {
-                            let v: syn::LitStr = inner.value()?.parse()?;
-                            id = Some(v.value());
-                        } else if inner.path.is_ident("Label") {
-                            let v: syn::LitStr = inner.value()?.parse()?;
-                            label = Some(v.value());
-                        }
-                        Ok(())
-                    })?;
+                if meta.path.is_ident("id") {
+                    let v: LitStr = meta.value()?.parse()?;
+                    id = Some(v.value());
+                } else if meta.path.is_ident("label") {
+                    let v: LitStr = meta.value()?.parse()?;
+                    label = Some(v.value());
                 }
                 Ok(())
             })?;
         }
-        Ok(StrumProps { id, label })
+        Ok(MenuProps { id, label })
     }
 }
 
