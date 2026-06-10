114e99a183a7b010e12df146efaf55dd88e9d17b
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2026-06-10 12:25:09 +0200

    chore(release): v0.0.14 (#67)
    * chore(release): v0.0.14
    
    * chore(release): update cask and update manifest to 0.0.14 [skip ci]

--- Casks/vmux.rb
+++ Casks/vmux.rb
@@ -1,8 +1,8 @@
 cask "vmux" do
-  version "0.0.13"
-  sha256 "b6b339890b74f4693e36674c3b6b14228d0af722d5cb2c940646fcf363854e16"
+  version "0.0.14"
+  sha256 "6e2006c23fc488a4d6eef1ef6c513d294ea60bf76a24bc515bdf6a203ce33cc9"
 
-  url "https://github.com/vmux-ai/vmux/releases/download/v0.0.13/Vmux_0.0.13_aarch64.dmg"
+  url "https://github.com/vmux-ai/vmux/releases/download/v0.0.14/Vmux_0.0.14_aarch64.dmg"
   name "Vmux"
   desc "AI-native workspace combining browser and terminal panes"
   homepage "https://vmux.ai"
